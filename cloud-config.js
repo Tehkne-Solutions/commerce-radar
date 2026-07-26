0be1fe21a4bec0cc443a5cfddf3e160a9419de6a
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-26 13:02:48 -0300

    chore: configure workspace versions table

--- cloud-config.js
+++ cloud-config.js
@@ -1,5 +1,6 @@
 window.COMMERCE_RADAR_CLOUD={
   url:'',
   publishableKey:'',
-  table:'commerce_radar_workspaces'
+  table:'commerce_radar_workspaces',
+  versionsTable:'commerce_radar_workspace_versions'
 };
